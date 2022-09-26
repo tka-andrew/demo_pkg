f7c4b3b31822dff258596ec593ebedd7f11a22c7
tka-andrew <kui.an.andrew.tan@gmail.com>
2022-09-26 21:45:17 +0800

    reduce magic number in test file

--- tests/test_add_two_ints_server.py
+++ tests/test_add_two_ints_server.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import rospy
 import unittest
+import random
 from demo_pkg.srv import AddTwoInts
 
 
@@ -8,16 +9,20 @@ class TestAddTwoIntsServer(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls) -> None:
+        max_int = pow(2, 64)/2
+        # as the add_two_ints currently cannot handle overflow,
+        # so we need to divide the max_int by 2
+        cls.max_random_int = max_int / 2
         # necessary to wait for service to ready
         rospy.wait_for_service('add_two_ints')
 
     def test_handle_add_two_ints(self):
-        inputs = [1, 2, 4, 5, 11, 100]
-        outputs = [100, 219, 214, 45, 1121, 1200]
-        expected_list = [101, 221, 218, 50, 1132, 1300]
+        A = [random.randint(-self.max_random_int, self.max_random_int) for i in range(10)]
+        B = [random.randint(-self.max_random_int, self.max_random_int) for i in range(10)]
+        expected_list = [A[i] + B[i] for i in range(10)]
         add_two_ints = rospy.ServiceProxy('add_two_ints', AddTwoInts)
-        for i in range(len(inputs)):
-            resp = add_two_ints(inputs[i], outputs[i])
+        for i in range(len(A)):
+            resp = add_two_ints(A[i], B[i])
             self.assertEqual(resp.sum, expected_list[i])
 
 
--- tests/test_io_listener.py
+++ tests/test_io_listener.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 
 import rospy
 import unittest
+import random
 from std_msgs.msg import UInt16
 
 from io_manager.io_listener import IoListener
@@ -12,12 +13,15 @@ class TestIoListener(unittest.TestCase):
 
     # reference: https://docs.python.org/3/library/unittest.html#unittest.TestCase.setUpClass
     @classmethod
-    def setUpClass(cls) -> None:
-        cls.input_pubs = [rospy.Publisher(
-            'io/input/port%d' % (i), UInt16, queue_size=10) for i in range(1, 9)]
-        cls.output_pubs = [rospy.Publisher(
-            'io/output/port%d' % (i), UInt16, queue_size=10) for i in range(1, 9)]
+    def setUpClass(cls):
         cls.io_listener = IoListener()
+        cls.num_of_ports = 8
+        cls.bits_per_port = 16
+        cls.max_int = pow(2, cls.bits_per_port)
+        cls.input_pubs = [rospy.Publisher(
+            'io/input/port%d' % (i), UInt16, queue_size=10) for i in range(1, cls.num_of_ports + 1)]
+        cls.output_pubs = [rospy.Publisher(
+            'io/output/port%d' % (i), UInt16, queue_size=10) for i in range(1, cls.num_of_ports + 1)]
         rospy.sleep(1)  # wait of initialization
 
     # reference: https://docs.python.org/3/library/unittest.html#unittest.TestCase.setUp
@@ -26,15 +30,15 @@ class TestIoListener(unittest.TestCase):
         self.io_listener.reset_inputs_outputs()
 
     def test_input_callback(self):
-        inputs = [801, 37, 55, 67, 94, 131, 153, 155]
-        for i in range(8):
+        inputs = [random.randint(0, self.max_int) for i in range(self.num_of_ports)]
+        for i in range(self.num_of_ports):
             self.input_pubs[i].publish(UInt16(data=inputs[i]))
             rospy.sleep(0.2)  # give some time for callback function to execute
         self.assertEqual(self.io_listener.get_inputs(), inputs)
 
     def test_output_callback(self):
-        outputs = [31, 13, 512, 127, 92, 121, 413, 515]
-        for i in range(8):
+        outputs = [random.randint(0, self.max_int) for i in range(self.num_of_ports)]
+        for i in range(self.num_of_ports):
             self.output_pubs[i].publish(UInt16(data=outputs[i]))
             rospy.sleep(0.2)  # give some time for callback function to execute
         self.assertEqual(self.io_listener.get_outputs(), outputs)
